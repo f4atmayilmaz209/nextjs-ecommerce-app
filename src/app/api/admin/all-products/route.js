4428148f9f003616c4055aa43b0634e1dc133597
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2023-09-28 14:15:23 +0300

    fixed

--- src/app/api/admin/all-products/route.js
+++ src/app/api/admin/all-products/route.js
@@ -1,7 +1,7 @@
 import connectToDB from "@/database";
 import { NextResponse } from "next/server";
 import Product from "@/models/product";
-
+import AuthUser from "@/middleware/AuthUser";
 
 export const dynamic="force-dynamic";
 
@@ -9,23 +9,32 @@ export async function GET(req){
 
     try{
         await connectToDB();
+        const isAuthUser=await AuthUser(req)
+        if(isAuthUser?.role==='admin'){
+            const extractAllproducts=await Product.find({})
+            console.log("gggghghghg")
+            console.log(extractAllproducts)
+            console.log("gggghghghg")
+            if(extractAllproducts){
+                return NextResponse.json({
+                    success:true,
+                    data:extractAllproducts
+                })
+            }else{
 
-        const extractAllproducts=await Product.find({})
-        console.log("gggghghghg")
-        console.log(extractAllproducts)
-        console.log("gggghghghg")
-        if(extractAllproducts){
-            return NextResponse.json({
-                success:true,
-                data:extractAllproducts
-            })
-        }else{
-
+                return NextResponse.json({
+                    success:false,
+                    status:204,
+                    message:'No products found',
+                })
+            }
+        }
+        else{
             return NextResponse.json({
                 success:false,
-                status:204,
-                message:'No products found',
+                message:"something went wrong!Please try again later",
             })
+
         }
 
 
