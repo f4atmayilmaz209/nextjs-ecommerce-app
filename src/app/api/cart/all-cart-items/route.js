d2e3cb629257f0f6a7aa221ce9b394984b46a2c9
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2023-09-28 22:27:55 +0300

    fi

--- src/app/api/cart/all-cart-items/route.js
+++ src/app/api/cart/all-cart-items/route.js
@@ -23,7 +23,7 @@ export async function GET(req){
             const extractAllCartItemsr=await Cart.find({userID:id})
             console.log(extractAllCartItemsr)
             console.log("re")
-            const extractAllCartItems=await Cart.find({userID:id}).populate('productID');
+            const extractAllCartItems=await Cart.findOne({userID:id}).populate('productID');
             console.log("extr")
             console.log(extractAllCartItems)
             console.log("extr")
