a92d0f2e94929ff805f02c9274ff47cc327ecad6
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2023-09-29 14:34:41 +0300

    qw

--- src/app/api/cart/all-cart-items/route.js
+++ src/app/api/cart/all-cart-items/route.js
@@ -13,6 +13,9 @@ export async function GET(req){
         if(isAuthUser){
             const {searchParams}=new URL(req.url);
             const id=searchParams.get('id')
+            console.log("id")
+            console.log(id)
+            console.log("id")
             // if(!id) return NextResponse.json({success:false,message:'please login in!'})
             const extractAllCartItems=await Cart.find({userID:id}).populate('productID')
             console.log("var")
