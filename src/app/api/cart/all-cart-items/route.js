0bc269d8ffa9bb7bfd6e7d08b2df4182f81651da
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2023-09-29 17:46:14 +0300

    qqqq

--- src/app/api/cart/all-cart-items/route.js
+++ src/app/api/cart/all-cart-items/route.js
@@ -18,20 +18,12 @@ export async function GET(req){
             console.log(id)
             console.log("id")
             // if(!id) return NextResponse.json({success:false,message:'please login in!'})
-            //const extractAllCartItems=await Cart.find({userID:id}).populate({path:"productID",model:"Products"})
+
             const yo=await Cart.find({userID:id})
             const r=yo
-            console.log("wwwwwwww")
-            console.log(r[0]["productID"].toString())
-            console.log("wwwwwwww")
             const extractAllCartItemsp=await Product.findById(r[0]["productID"].toString())
-            console.log(extractAllCartItemsp)
-            console.log("extractAllCartItemsp")
             const extractAllCartItems=await Cart.find({userID:id}).populate('productID')
-            //const extractAllCartItems=await Cart.find({}).populate('productID')
-            console.log("var")
-            console.log(extractAllCartItems)
-            console.log("var")
+
             if(extractAllCartItems){
                 return NextResponse.json({success:true,data:extractAllCartItems})
             }
